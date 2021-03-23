cf39dc663dfbfc27a39c70a635fd4a620953bb14
Frederick William Edlim <frederickwilliamedlim@gmail.com>
2021-03-23 14:26:19 +0700

    selesai soal 3a

--- soal3/soal3a.sh
+++ soal3/soal3a.sh
@@ -1,1 +1,36 @@
 #!/bin/bash
+#crontab :
+# 0 20 1/7,2/4 * * 
+file_name_template="Koleksi_"
+download_url="https://loremflickr.com/320/240/kitten"
+
+
+file_number=1
+# download semua gambar
+for ((num=1; num<=23; num=$num+1))
+do
+    # download gambar
+    printf -v file_number_string "%02d" $file_number
+    file_name="$file_name_template$file_number_string"
+    wget -O "$file_name" $download_url -a "Foto.log"
+
+    # cek gambar duplikat atau nggak
+    grep 'Location: /cache/resized' "Foto.log" > "location.log"
+
+    last_location_downloaded=$(cat location.log | tail -1)
+    echo $last_location_downloaded
+    match_count=$(grep -cF "$last_location_downloaded" "location.log")
+    echo $match_count
+    
+    if [ $match_count -ne 1 ]
+    then
+        rm $file_name
+        continue
+    fi
+    
+    file_number=$(($file_number + 1))
+done
+
+rm "Foto.log"
+
+echo "done"
